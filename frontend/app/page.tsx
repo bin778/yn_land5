189afbc0df9a91a77130f3c15dbc6b3ad9536629
brown-chip <bin778@naver.com>
2026-09-01 15:24:01 +0900

    fix: Change section layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
         <StoryIntroSection />
         <ProblemSection />
         <TimeProtectionSection />
-        <ConsultSection />
         <ProcessSection />
         <ValueSection />
         <LawyersSection />
+        <ConsultSection />
         <FaqSection />
         <FinalCtaSection />
       </main>
